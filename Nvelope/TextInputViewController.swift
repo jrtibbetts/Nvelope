523010b8d56c39579a4db3d5bd4732d6de5f84a3
Jason R Tibbetts <jason@tibbetts.net>
2020-04-19 21:01:39 -0400

    UI changes that I made a long time ago. I don't even know what they are anymore.

--- Nvelope/TextInputViewController.swift
+++ Nvelope/TextInputViewController.swift
@@ -10,28 +10,129 @@ open class TextInputViewController: UIViewController {
     // MARK: - Outlets
 
     /// The main text view.
-    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var textView: UITextView! {
+        didSet {
+            if let textView = textView {
+                keyboardObserver = KeyboardObserver(scrollView: textView)
+            }
+        }
+    }
 
-    /// The web view that displays the contents of a URL that was entered
-    /// manually.
-    @IBOutlet weak var webView: UIWebView!
+    // MARK: - Other properties
 
-    /// The text field that's in the instructional view that "floats" over the
-    /// text or web view. When a user enters a URL and hits enter, the
-    /// floating view fades out and topUrlField springs to the top of the view.
-    @IBOutlet weak var floatingUrlField: UITextField!
+    /// The utility that adjusts the text view's bottom content inset
+    /// when the keyboard is shown or hidden.
+    private var keyboardObserver: KeyboardObserver?
 
-    /// The text field that's at the top of the web view.
-    @IBOutlet weak var topUrlField: UITextField!
+}
 
-    /// The grid of part-of-speech buttons that can be toggled to highlight
-    /// tokens of the desired type in the text or web view.
-    @IBOutlet weak var tokenTypeButtons: UICollectionView!
+/// A utility class that manages a scroll view and adjusts its content insets
+/// appropriately when the keyboard is shown or hidden. By default, this
+/// adjustment happens just before the keyboard is shown and just after it's
+/// hidden, but subclasses can override the four `show*()` and `hide*()`
+/// methods to change this behavior.
+open class KeyboardObserver: NSObject {
 
-    override open func viewDidLoad() {
-        super.viewDidLoad()
+    /// The scroll view that this observer manipulates.
+    private var scrollView: UIScrollView
 
-        
+    /// The insets of the scroll view's contents *before* the keyboard is
+    /// shown. These are captured just before the keyboard is shown and
+    /// restored right after it's hidden.
+    private var originalContentInsets = UIEdgeInsets()
+
+    /// Construct a keyboard observer and register for notifications.
+    ///
+    /// - parameter scrollView: The scroll view that this observer manipulates.
+    public init(scrollView: UIScrollView) {
+        self.scrollView = scrollView
+        super.init()
+        registerForNotifications()
+    }
+
+    deinit {
+        unregisterFromNotifications()
+    }
+
+    /// Called when the `NSNotification.Name.UIKeyboardWillShow` notification
+    /// is received. This will store the scroll view's current `contentInsets`
+    /// and then adjust the bottom inset by increasing the bottom inset by the
+    /// height of the keyboard.
+    ///
+    /// - parameter notification: The `NSNotification.Name.UIKeyboardWillShow`
+    ///   notification.
+    @objc open func showKeyboard(notification: Notification) {
+        let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as! CGRect
+        let keyboardSize = keyboardFrame.size
+        originalContentInsets = scrollView.contentInset
+        let newEdgeInsets = UIEdgeInsets(top: originalContentInsets.top,
+                                         left: originalContentInsets.left,
+                                         bottom: keyboardSize.height + originalContentInsets.bottom,
+                                         right: originalContentInsets.right)
+        scrollView.contentInset = newEdgeInsets
+        scrollView.scrollIndicatorInsets = newEdgeInsets
+    }
+
+    /// Called when the `NSNotification.Name.UIKeyboardDidShow` notification is
+    /// received. This implementation does nothing.
+    ///
+    /// - parameter notification: The `NSNotification.Name.UIKeyboardDidShow`
+    ///   notification.
+    @objc open func showedKeyboard(notification: Notification) {
+        // Nada.
+    }
+
+    /// Called when the `NSNotification.Name.UIKeyboardWillHide` notification
+    /// is received. This implementation does nothing.
+    ///
+    /// - parameter notification: The `NSNotification.Name.UIKeyboardWillHide`
+    ///   notification.
+    @objc open func hideKeyboard(notification: Notification) {
+        // Zilch.
+    }
+
+    /// Called when the `NSNotification.Name.UIKeyboardDidHide` notification
+    /// is received. This will restore the scroll view's original content
+    /// insets.
+    ///
+    /// - parameter notification: The `NSNotification.Name.UIKeyboardWillHide`
+    ///   notification.
+    @objc open func hidKeyboard(notification: Notification) {
+        scrollView.contentInset = originalContentInsets
+        scrollView.scrollIndicatorInsets = originalContentInsets
+    }
+
+    // MARK: - Registration
+
+    /// Register with the default `NotificationCenter` to receive all four
+    /// kinds of keyboard-visibility events.
+    private func registerForNotifications() {
+        let notifier = NotificationCenter.default
+        notifier.addObserver(self,
+                             selector: #selector(showKeyboard(notification:)),
+                             name: UIResponder.keyboardWillShowNotification,
+                             object: nil)
+        notifier.addObserver(self,
+                             selector: #selector(showedKeyboard(notification:)),
+                             name: UIResponder.keyboardDidShowNotification,
+                             object: nil)
+        notifier.addObserver(self,
+                             selector: #selector(hideKeyboard(notification:)),
+                             name: UIResponder.keyboardWillHideNotification,
+                             object: nil)
+        notifier.addObserver(self,
+                             selector: #selector(hidKeyboard(notification:)),
+                             name: UIResponder.keyboardDidHideNotification,
+                             object: nil)
+    }
+
+    /// Unregister from all keyboard-visibility events.
+    private func unregisterFromNotifications() {
+        let notifier = NotificationCenter.default
+        notifier.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        notifier.removeObserver(self, name: UIResponder.keyboardDidShowNotification, object: nil)
+        notifier.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+        notifier.removeObserver(self, name: UIResponder.keyboardDidHideNotification, object: nil)
     }
 
 }
